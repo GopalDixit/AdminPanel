ec8d808cc04835ca72ff83ffceca79049cf0e756
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-08-09 00:23:00 +0530

    Update HomePageService.jsx

--- frontend/src/components/homeDashboard/HomePageService.jsx
+++ frontend/src/components/homeDashboard/HomePageService.jsx
@@ -7,7 +7,7 @@ const ServiceList = () => {
   const [editService, setEditService] = useState(null);
 
   useEffect(() => {
-    axios.get('https://adminpanel-backend-aijl.onrender.com/api/services')
+    axios.get('https://adminpanel-fback.onrender.com/api/services')
       .then(response => setServices(response.data))
       .catch(error => console.error(error));
   }, []);
@@ -20,7 +20,7 @@ const ServiceList = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (editService) {
-      axios.put(`https://adminpanel-backend-aijl.onrender.com/api/services/${editService._id}`, formData)
+      axios.put(`https://adminpanel-fback.onrender.com/api/services/${editService._id}`, formData)
         .then(response => {
           setServices(services.map(service => service._id === editService._id ? response.data : service));
           setFormData({ title: '', description: '', image: '', icon: '' });
@@ -30,7 +30,7 @@ const ServiceList = () => {
           console.error('Error updating service:', error.response ? error.response.data : error.message);
         });
     } else {
-      axios.post('https://adminpanel-backend-aijl.onrender.com/api/services', formData)
+      axios.post('https://adminpanel-fback.onrender.com/api/services', formData)
         .then(response => {
           setServices([...services, response.data]);
           setFormData({ title: '', description: '', image: '', icon: '' });
@@ -47,7 +47,7 @@ const ServiceList = () => {
   };
 
   const handleDelete = (id) => {
-    axios.delete(`https://adminpanel-backend-aijl.onrender.com/api/services/${id}`)
+    axios.delete(`https://adminpanel-fback.onrender.com/api/services/${id}`)
       .then(() => setServices(services.filter(service => service._id !== id)))
       .catch(error => console.error(error));
   };
